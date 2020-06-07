4984b41b8ecd43efbcc0edc77baa0cc365fe4062
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-07 20:53:47 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+class School
+attr_accessor :school_name, :roster
+
+def initialize(school_name)
+@school_name = school_name
+@roster = {}
+end
+
+
+
+def add_student(name, grade)
+if @roster[grade]
+   @roster[grade] << name
+  else
+   @roster[grade] = []
+   @roster[grade] << name
+end
+end
+
+def grade(number)
+@roster[number]
+end
+
+def sort
+new_hash = {}
+newer = []
+@roster.each do |key, value_array|
+newer = value_array.sort do |a,b|
+  a<=>b
+end
+new_hash[key] = []
+new_hash[key] = newer
+end
+new_hash
+end
+
+end
