395964be147034cf324c9e60e413f16d86994783
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-07 20:38:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+class School
+attr_accessor :school_name, :roster
+
+def initialize(school_name)
+@school_name = school_name
+@roster = {}
+end
+
+
+
+def add_student(name, grade)
+if @roster[grade]
+   @roster[grade] << name
+  else
+   @roster[grade] = []
+   @roster[grade] << name
+end
+end
+
+def grade(number)
+@roster[number]
+end
+
+def sort
+binding.pry
+end
+
+end
---
+++ lib/school_test.rb
@@ -0,0 +1,4 @@
+require "school.rb"
+
+school = School.new
+puts School.roster
